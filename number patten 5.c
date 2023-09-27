35680fd65ebedc9e11e498802b652adddf3ea3d3
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-27 17:22:22 +0530

    Create number patten 5.c
    54321
    5432
    543
    54
    5

---
+++ number patten 5.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+ main() 
+{
+  int i, j, n;
+  printf("Enter the number of rows: ");
+  scanf("%d", &n);
+
+  for (i = 5; i >= 1; i--) 
+{
+    for (j = i; j <=5; j++) 
+    {
+      printf("%d",j);
+    }
+    printf("\n");
+  }
+}
