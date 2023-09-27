b23f910b07f6e4a92543548207ad3c2dfe2ba1cd
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-27 17:35:09 +0530

    Create two pyramid.c
    *
       ***
      *****
     *******
    *********
    *********
     *******
      *****
       ***
        *
    *
       ***
      *****
     *******
    *********
    *********
     *******
      *****
       ***
        *

---
+++ two pyramid.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main() 
+{
+  int i,j,k,n;
+
+  printf("Enter the number of rows: ");
+  scanf("%d", &n);
+
+  for (i = 1; i<=n; i++) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    for (i = n; i>=1; i--) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    for (i = 1; i<=n; i++) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    for (i = n; i>=1; i--) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+  for (i = 1; i<=n; i++) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    for (i = n; i>=1; i--) 
+{
+    for (j =1;j<=n-i; j++) 
+    {
+      printf(" ");
+    }
+    for(k=1;k<=(2*i-1);k++) 
+    {
+    printf("*");
+    }
+    printf("\n");
+    }
+    }
