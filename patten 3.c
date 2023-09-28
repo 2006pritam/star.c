6b168762cb02d7964603e4e2c0b620e78de6cd39
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-28 08:32:21 +0530

    Create patten 3.c
    A B C D 
    A B C D 
    A B C D 
    A B C D

---
+++ patten 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() 
+{
+  int i,j,n;
+printf("enter the number");
+scanf("%d",&n);
+
+  for (i= 65; i<= n; i++) 
+{
+    for (j =65; j<=68; j++)
+     {
+      printf("%c ",j);
+    }
+    printf("\n");
+  }
+}
