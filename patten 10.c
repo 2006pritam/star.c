05a0050894bb1c486122e3dfeb7328509f23c6ec
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-28 08:48:56 +0530

    Create patten 10.c
    A
    BB
    CCC
    DDDD
    EEEEE
    DDDD
    CCC
    BB
    A

---
+++ patten 10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+main() 
+{
+    int i,j,k,n,a=64;
+    printf("enter the number of line");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",a+i);
+        }
+        printf("\n");
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",a+i);
+        }
+        printf("\n");
+    }
+}
