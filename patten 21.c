f90bf11cc100ab7aea7b31dd81f1aa481f79cf2a
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-28 18:11:06 +0530

    Create patten 21.c
    1
    1*
    1*3
    1*3*
    1*3*5

---
+++ patten 21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+  int i,j;
+  for (i = 1; i <= 5; i++) 
+  {
+    for (j = 1; j <=i ; j++)
+      if(j%2==0)
+      {
+      printf("*"); 
+      }
+    else
+    {
+    printf("%d",j);
+    }
+    printf("\n");
+  }
+  }
